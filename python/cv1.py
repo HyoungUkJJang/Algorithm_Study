9bc0629dafa939883d963367f62c17c661aaba43
HyoungUkJJang <guddnr1451@gmail.com>
2021-05-06 07:10:44 +0900

    기본 정수, 문자열, 인덱스, 슬라이싱, 포맷팅 실습 !

--- python/cv1.py
+++ python/cv1.py
@@ -1,1 +1,43 @@
-print("hello python!")
+#Integer
+# a=100
+# b=200
+# c=-300
+# print(a,b,c)
+
+#String
+# str="HyoungUkJJAng"
+# str1="\"HyoungUkJJAng\"\nStudy"
+# str2='''HyoungUkJJang
+# Study
+# good'''
+# print(str2)
+
+#String2
+# str1 = "Kim"
+# str2 = "HyoungUk"
+# print(str1+" "+str2)
+# print(str1*2)
+
+#String2 Ex
+# print("="*30)
+# print("Kim")
+# print("="*30)
+
+#String Length / slice
+# str1 = "hihi"
+# print(len(str1))
+# print(str1[:2])
+# print(str1[0]+" "+str1[-0])
+# print(str1[-1])
+# print(str1[-3])
+# str2 = "Hi my name is HyoungUk Kim"
+# print(str2[0:3]) #0~2
+# print(len(str2))
+# print(str2[:])
+# print(str2[14:-1]) # 14 ~ -2
+
+#String format
+num=6
+day="mon"
+fm = "now %d and %s days" %(num,day)
+print(fm)
