7c1c996f7b4212e511e202385eb65d526ff67603
HyoungUk Kim <guddnr1451@gmail.com>
2021-05-13 07:44:57 +0900

    문자열 공백, 변환, 자르기 공부
    문자열 실습

---
+++ python/ch3.py
@@ -0,0 +1,25 @@
+#String practice
+
+# strip
+# str = "  ghig     "
+# print(str.strip())
+
+# # rstrip()
+# str="  HyoungUk   "
+# print(str.rstrip())
+
+# lstrip()
+# str="  HyoungUk   "
+# print(str.lstrip())
+
+# replace
+# str = "hi hello HyounUk"
+# print(str.replace("hi", "change"))
+
+# split
+# str = "hi hello haha hoho"
+# str = str.split()
+# print(str)
+# str2 = "a:b:c:d"
+# str2 = str2.split(":")
+# print(str2)
