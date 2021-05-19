f8fe6b4a8114c285949b6d21a4eda426d2ed68f4
HyoungUk Kim <guddnr1451@gmail.com>
2021-05-19 15:53:39 +0900

    단계별 풀어보기 조건문 1~5번 풀어보기

---
+++ 백준/조건문/if_1.py
@@ -0,0 +1,55 @@
+# 1
+# A, B = map(int,input().split())
+# if A > B :
+#     print('>')
+# elif A < B :
+#     print('<')
+# elif A == B :
+#     print('==')
+
+#2
+# A = int(input())
+# if A >= 90 and A<=100 :
+#     print('A')
+# elif A >=80 and A<90 :
+#     print('B')
+# elif A >=70 and A<80 :
+#     print('C')
+# elif A >=60 and A<70 :
+#     print('D')
+# else :
+#     print('F')
+ 
+# 3
+# A = int(input())
+# if A % 400 == 0 :
+#     print(1)
+# elif A % 4 == 0 and A % 100 != 0 :
+#     print(1)
+# else :
+#     print(0)
+
+# 4
+# x=int(input())
+# y=int(input())
+# if x > 0 and y > 0 :
+#     print(1)
+# elif x < 0 and y > 0 :
+#     print(2)
+# elif x < 0 and y < 0 :
+#     print(3)
+# else :
+#     print(4)
+
+# # 5
+# H,M = map(int,input().split())
+# if M >= 45 :
+#     M = M - 45
+#     print(H,M)
+# elif M < 45 :
+#     if H == 0 :
+#         H = 24
+#     M = M + 15
+#     H = H - 1
+#     print(H,M)
+    
