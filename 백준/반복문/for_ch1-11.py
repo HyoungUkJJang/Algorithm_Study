e795556c87b13ad338d2af230c08025daff84dcf
HyoungUk Kim <guddnr1451@gmail.com>
2021-05-19 21:59:46 +0900

    백준 반복분 기초 1 ~ 11

---
+++ 백준/반복문/for_ch1-11.py
@@ -0,0 +1,63 @@
+# 1
+# N = int(input())
+# for i in range(1,10) :
+#     print("%d * %d = %d"%(N,i,N*i))
+    
+# 2
+# T = int(input())
+# for i in range(0,T) :
+#     A, B = map(int,input().split())
+#     print(A+B)
+    
+# 3
+# n = int(input())
+# sum = 0
+# for i in range(1,n+1) :
+#         sum+=i
+# print(sum)
+
+# 4
+# import sys
+# T = int(input())
+# for i in range(T) :
+#     a,b = map(int, sys.stdin.readline().split())
+#     print(a+b)
+    
+# 5
+# n = int(input())
+# for i in range(1,n+1) :
+#     print(i)
+
+# 6
+# n = int(input())
+# for i in range(n,0,-1) :
+#     print(i)
+    
+# 7
+# T = int(input())
+# for i in range(1,T+1) :
+#     A,B = map(int,input().split())
+#     print("Case #%d: %d"%(i,A+B))
+    
+# 8
+# T = int(input())
+# for i in range(1,T+1) :
+#     A,B = map(int,input().split())
+#     print("Case #%d: %d + %d = %d"%(i,A,B,A+B))
+    
+# 9
+# N = int(input())
+# for i in range(1,N+1) :
+#     print('*'*i)
+    
+# 10
+# n = int(input())
+# for i in range(1, n + 1):
+#     print(" " * (n - i) + "*" * i)
+    
+# 11
+# N,X = map(int,input().split())
+# A = list(map(int,input().split()))
+# for i in range(N) :
+#     if X > A[i] :
+#         print(A[i],end=" ") 
