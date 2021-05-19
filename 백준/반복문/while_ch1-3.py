29fbb971952bcc3cb887b2ac54de3609b0d030ac
HyoungUk Kim <guddnr1451@gmail.com>
2021-05-20 08:21:34 +0900

    Create while_ch1-3.py

---
+++ 백준/반복문/while_ch1-3.py
@@ -0,0 +1,16 @@
+# 1
+# while True :
+#     A, B = map(int,input().split())
+#     if A == 0 and B == 0 :
+#         break
+#     print(A+B)
+    
+# 2
+# while True :
+#     try :
+#         A, B = map(int,input().split())
+#         print(A+B)
+#     except :
+#         print('error')
+
+  
