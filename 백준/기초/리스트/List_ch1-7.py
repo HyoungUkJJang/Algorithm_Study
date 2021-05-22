ebd225f599676e7afeb093fdb0f35f16ede38b65
HyoungUk Kim <guddnr1451@gmail.com>
2021-05-22 09:42:32 +0900

    Create List_ch1-7.py

---
+++ 백준/기초/리스트/List_ch1-7.py
@@ -0,0 +1,29 @@
+# 1
+# N = int(input())
+# A = list(map(int,input().split()))
+# print(min(A),max(A))
+
+# 2
+# a = []
+# for i in range(0,9) :
+#     n = int(input())
+#     a.append(n)
+# print(max(a))
+# print(a.index(max(a))+1)
+
+# # 3 
+# i = int(input())
+# j = int(input())
+# b = int(input())
+# a = i*j*b
+# print(str(a).count('0'))
+# print(str(a).count('1'))
+# print(str(a).count('2'))
+# print(str(a).count('3'))
+# print(str(a).count('4'))
+# print(str(a).count('5'))
+# print(str(a).count('6'))
+# print(str(a).count('7'))
+# print(str(a).count('8'))
+# print(str(a).count('9'))
+
