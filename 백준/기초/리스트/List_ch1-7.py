2e219354e31d7fd2e31fb52622ebebb960d2ad6a
HyoungUk Kim <guddnr1451@gmail.com>
2021-05-23 13:46:30 +0900

    Update List_ch1-7.py

--- 백준/기초/리스트/List_ch1-7.py
+++ 백준/기초/리스트/List_ch1-7.py
@@ -11,7 +11,7 @@
 # print(max(a))
 # print(a.index(max(a))+1)
 
-# # 3 
+#  3 
 # i = int(input())
 # j = int(input())
 # b = int(input())
@@ -27,3 +27,38 @@
 # print(str(a).count('8'))
 # print(str(a).count('9'))
 
+# 4
+# n = []
+# for _ in(range(10)) :
+#     a=int(input())
+#     b = a % 42
+#     n.append(b)
+# s = set(n)
+# print(len(s))
+
+# 5
+# n = int(input())
+# s = list(map(int,input().split()))
+# summ=0
+# m = max(s)
+# for i in range(0,len(s)) :
+#      s[i] = s[i] / m * 100
+#      summ += s[i]
+# print(summ/n)
+
+# 6
+# n = int(input())
+
+# for c in range(0,n) :
+#     str = list(input())
+#     pivot = 1
+#     sum = 0
+#     for i in str :
+#         if i == 'O' :
+#             sum = sum+pivot
+#             pivot = pivot + 1
+#         else :
+#             pivot = 1 
+#     print(sum)
+    
+  
