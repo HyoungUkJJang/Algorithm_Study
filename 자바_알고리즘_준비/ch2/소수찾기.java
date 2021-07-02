0caee8467436cc87c0b87b01abfd664367b63db7
HyoungUk <guddnr1451@gmail.com>
2021-07-02 23:08:35 +0900

    Update 소수찾기.java

--- 자바_알고리즘_준비/ch2/소수찾기.java
+++ 자바_알고리즘_준비/ch2/소수찾기.java
@@ -25,3 +25,32 @@ public class Main {
 
     }
 }
+/*
+// 에라토스테네스
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int n){
+        int[] ch = new int[n+1];
+        int answer=0;
+        for(int i=2;i<=n;i++) {
+            if(ch[i]==0) {
+                answer++;
+                for(int j=i; j<=n; j=j+i) ch[j] = 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        System.out.println(T.solution(n));
+
+    }
+}
+
+*/
