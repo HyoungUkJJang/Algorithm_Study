cfbacccb610cffbad2e7cd91c7086db0a84f7495
HyoungUk <guddnr1451@gmail.com>
2021-07-02 22:59:23 +0900

    Update 피보나치수열.java

--- 자바_알고리즘_준비/ch2/피보나치수열.java
+++ 자바_알고리즘_준비/ch2/피보나치수열.java
@@ -25,3 +25,37 @@ public class Main {
 
     }
 }
+/* 배열을 안쓸경우
+import java.util.Scanner;
+
+public class Main {
+
+    public void solution(int n){
+        int a=1,b=1,c;
+        System.out.print(a+" "b+" ");
+        for(int i=2;i<n;i++){
+            c=a+b;
+            System.out.println(c+" ");
+            a=b;
+            b=c;
+        }
+        int[] answer = new int[n];
+        answer[0]=1;
+        answer[1]=1;
+        answer[2]=2;
+        for(int i=3;i<n;i++) answer[i] = answer[i-1]+answer[i-2];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        T.solution(n)
+
+    }
+}
+
+*/
