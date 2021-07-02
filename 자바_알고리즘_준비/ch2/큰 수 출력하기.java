30c87553fb263e99dabf3672e151807dec0280aa
HyoungUk <guddnr1451@gmail.com>
2021-07-02 21:47:56 +0900

    Update 큰 수 출력하기.java

--- 자바_알고리즘_준비/ch2/큰 수 출력하기.java
+++ 자바_알고리즘_준비/ch2/큰 수 출력하기.java
@@ -28,3 +28,36 @@ public class Main {
 
     }
 }
+
+/* 모범
+
+import java.util.Scanner;
+
+public class Main {
+
+    public ArrayList<Integer> solution(int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+
+            for(int i=1;i<arr.length;i++){
+                if(arr[i-1] < arr[i]) answer.add(arr[i]);
+            }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = kb.nextInt();
+        }
+        for(int x : T.soulution(n,arr)){
+        System.out.println(x+" ");
+        }
+
+    }
+}
+
+*/
