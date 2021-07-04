7d61a6fb0bc28a4e5044617757c4601112838f78
HyoungUk <guddnr1451@gmail.com>
2021-07-04 11:55:27 +0900

    Update 두배열 합치기.java

--- 자바_알고리즘_준비/ch3/두배열 합치기.java
+++ 자바_알고리즘_준비/ch3/두배열 합치기.java
@@ -30,3 +30,49 @@ class Main {
 
     }
 }
+/*인상적인 답
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public ArrayList<Integer> solution(int n, int m,int[] arr1, int[] arr2){
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1=0,p2=0;
+
+        while(p1<n && p2<m) {
+            if(arr1[p1] > arr2[p2])
+                answer.add(arr2[p2++]);
+            else
+                answer.add(arr1[p1++]);
+        }
+        if(p1==n) {
+            for(int i=p2; i<m;i++) answer.add(arr2[i]);
+        }
+        else if(p2==n) {
+            for(int i=p1; i<n;i++) answer.add(arr1[i]);
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr1 = new int[n];
+        for(int i=0;i<n;i++) arr1[i] = kb.nextInt();
+
+        int m = kb.nextInt();
+        int[] arr2 = new int[m];
+        for(int i=0;i<m;i++) arr2[i] = kb.nextInt();
+
+        for(int s : T.solution(n,m,arr1,arr2)) {
+            System.out.print(s+" ");
+        }
+
+    }
+}
+*/
