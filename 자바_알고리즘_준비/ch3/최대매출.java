ff7c440ed18eab7d68afe413566a7016f3b1e028
HyoungUk <guddnr1451@gmail.com>
2021-07-04 12:53:43 +0900

    Update 최대매출.java

--- 자바_알고리즘_준비/ch3/최대매출.java
+++ 자바_알고리즘_준비/ch3/최대매출.java
@@ -29,3 +29,66 @@ public class Main {
 
     }
 }
+// 내가 수정한답
+/*
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int[] arr1, int k){
+        int answer = 0;
+        for(int i=0;i<k;i++) answer+=arr1[i];
+        for(int i=k,j=k-1,n=k-2; i<arr1.length;i++,j++,n++) {
+            int sum=arr1[i] + arr1[j] + arr1[n];
+            if(sum>answer) answer=sum;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr1 = new int[n];
+        for(int i=0;i<n;i++) arr1[i] = kb.nextInt();
+
+        System.out.println(T.solution(arr1,k));
+
+    }
+}
+*/
+
+/* 이상적인 답
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int[] arr1, int k){
+        int answer = 0,sum=0;
+        for(int i=0;i<k;i++) sum+=arr1[i];
+        answer=sum;
+        for(int i=k;i<arr1.length;i++) {
+            sum+=(arr1[i]-arr1[i-k]);
+            answer = Math.max(answer,sum);
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr1 = new int[n];
+        for(int i=0;i<n;i++) arr1[i] = kb.nextInt();
+
+        System.out.println(T.solution(arr1,k));
+
+    }
+}
+
+*/
