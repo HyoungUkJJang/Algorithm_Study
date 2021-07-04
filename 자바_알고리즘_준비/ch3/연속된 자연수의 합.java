cc27c6aada28b711300ba8b3f332340a6aacd780
HyoungUk <guddnr1451@gmail.com>
2021-07-04 14:22:18 +0900

    방법이 3개

--- 자바_알고리즘_준비/ch3/연속된 자연수의 합.java
+++ 자바_알고리즘_준비/ch3/연속된 자연수의 합.java
@@ -68,3 +68,35 @@ public class Main {
     }
 }
 */
+/* 수학적인 방법을 이용
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int k){
+        int answer = 0, cnt=1;
+
+        k--;
+        while(k>0){
+            cnt++;
+            k=k-cnt;
+            if(k/cnt==0) answer++;
+        }
+
+        return answer;
+        }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        //   int n = kb.nextInt();
+        int k = kb.nextInt();
+        //    int[] arr1 = new int[n];
+        //    for(int i=0;i<n;i++) arr1[i] = kb.nextInt();
+
+        System.out.println(T.solution(k));
+
+    }
+}
+*/
