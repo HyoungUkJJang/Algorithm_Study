c0422c2f74c96866770dd1becb9a0808b9bc9823
HyoungUk <guddnr1451@gmail.com>
2021-07-04 13:57:58 +0900

    Update 연속된 자연수의 합.java

--- 자바_알고리즘_준비/ch3/연속된 자연수의 합.java
+++ 자바_알고리즘_준비/ch3/연속된 자연수의 합.java
@@ -33,3 +33,38 @@ public class Main {
 
     }
 }
+/* 시간복잡도 O(n)
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int k){
+        int answer = 0, lt=1;
+        int sum=0;
+        int m=k/2+1;
+        // n을 2로나눈 몫까지만 더해도된다..
+        for(int rt=1;rt<=m;rt++) {
+            sum+=rt;
+            if(sum==k) answer++;
+            while(sum>=k) {
+                sum-=lt++;
+                if(sum==k) answer++;
+              }
+            }
+        return answer;
+        }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        //   int n = kb.nextInt();
+        int k = kb.nextInt();
+        //    int[] arr1 = new int[n];
+        //    for(int i=0;i<n;i++) arr1[i] = kb.nextInt();
+
+        System.out.println(T.solution(k));
+
+    }
+}
+*/
