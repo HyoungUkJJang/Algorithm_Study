cb4263d0bf33e2e80808313b26fb4dfd3c3c65aa
HyoungUk <guddnr1451@gmail.com>
2021-07-05 15:59:20 +0900

    Update 크레인 인형뽑기.java

--- 자바_알고리즘_준비/ch5/크레인 인형뽑기.java
+++ 자바_알고리즘_준비/ch5/크레인 인형뽑기.java
@@ -50,3 +50,50 @@ public class Main {
         System.out.println(T.solution(arr,move));
     }
 }
+/*
+import java.util.*;
+
+public class Main {
+
+    public int solution(int[][] arr,int[] move) {
+        int answer=0;
+        Stack<Integer> stack = new Stack<>();
+        for(int x : move) {
+            for(int i=0;i<arr.length;i++) {
+                if(arr[i][x-1] != 0) {
+                    int tmp = arr[i][x-1];
+                    arr[i][x-1] = 0;
+                    if(!stack.isEmpty() && tmp==stack.peek()) {
+                        answer+=2;
+                        stack.pop();
+                    }
+                    else stack.push(tmp);
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i=0;i<n;i++){
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+
+        int move_count = kb.nextInt();
+        int[] move = new int[move_count];
+        for (int i = 0; i < move_count; i++) {
+            move[i] = kb.nextInt();
+        }
+
+        System.out.println(T.solution(arr,move));
+    }
+}
+*\/
