ce16e07f9a368dc98853e326a8691d72551ed72d
HyoungUk <guddnr1451@gmail.com>
2021-07-05 15:05:57 +0900

    Update 올바른 괄호.java

--- 자바_알고리즘_준비/ch5/올바른 괄호.java
+++ 자바_알고리즘_준비/ch5/올바른 괄호.java
@@ -24,3 +24,33 @@ public class Main {
         System.out.println(T.solution(str));
     }
 }
+
+/* 답
+import java.util.*;
+
+public class Main {
+
+    public String solution(String s) {
+        char[] arr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for(char x : arr) {
+            if(x == '(') stack.push(x);
+            else {
+              if(stack.isEmpty()) return "NO";
+              stack.pop();
+            }
+        }
+        if(stack.isEmpty()) return "YES";
+        
+        return "NO";
+    }
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
+*/
