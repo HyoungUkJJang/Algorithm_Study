086b0ec0777d2efb05c13f8604dd71255aa133cf
HyoungUk <guddnr1451@gmail.com>
2021-07-05 22:46:38 +0900

    Create 버블정렬.java

---
+++ 자바_알고리즘_준비/ch6/버블정렬.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+public class Main {
+
+    public int[] solution(int[] arr)
+    {
+        for(int i=0;i<arr.length;i++) {
+            int tmp=0;
+            for(int j=0;j<arr.length-i-1;j++) {
+                if(arr[j] > arr[j+1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for(int x : T.solution(arr)) {
+            System.out.print(x+" ");
+        }
+    }
+}
