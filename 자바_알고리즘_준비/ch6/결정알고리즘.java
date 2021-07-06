dfef5f02f71e71d76998321d6e303a8c1c08a88a
HyoungUk <guddnr1451@gmail.com>
2021-07-06 16:01:15 +0900

    Update 결정알고리즘.java

--- 자바_알고리즘_준비/ch6/결정알고리즘.java
+++ 자바_알고리즘_준비/ch6/결정알고리즘.java
@@ -38,3 +38,55 @@ public class Main {
         System.out.print(T.solution(arr,k));
     }
 }
+/* 정답
+import java.util.*;
+
+public class Main {
+
+    public int count(int[] arr, int capacity){
+        int cnt=1;
+        int sum=0;
+        for(int i=0;i<arr.length;i++) {
+            if(sum+arr[i]>capacity) {
+                cnt++;
+                sum=arr[i];
+            }
+            else sum+=arr[i];
+
+        }
+        return cnt;
+    }
+
+    public int solution(int[] arr, int k) {
+
+        int answer=0;
+        int lt=Arrays.stream(arr).max().getAsInt();
+        int rt=Arrays.stream(arr).sum();
+
+        while(lt<=rt){
+            int mid=(lt+rt)/2;
+            if(count(arr,mid)<=k) {
+                answer=mid;
+                rt=mid-1;
+            }
+            else{
+                lt=mid+1;
+            }
+
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        //T.solution(arr,k);
+        System.out.print(T.solution(arr,k));
+    }
+}
+*/
