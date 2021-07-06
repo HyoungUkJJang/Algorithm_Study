f9810e1749e369c5e9bba6f4a9690f278b31ee92
HyoungUk <guddnr1451@gmail.com>
2021-07-06 15:21:37 +0900

    Update 이분검색.java

--- 자바_알고리즘_준비/ch6/이분검색.java
+++ 자바_알고리즘_준비/ch6/이분검색.java
@@ -24,3 +24,36 @@ public class Main {
         System.out.println(T.solution(arr, k));
     }
 }
+/* 이게 이분검색. 내가한건 순차탐색이엿음
+import java.util.*;
+public class Main {
+
+    public int solution(int[] arr, int k)
+    {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt=0,rt=arr.length-1;
+
+        for(int i=0;i<arr.length;i++) {
+            int mid = (lt+rt) / 2;
+            if(arr[mid] == k) return mid+1;
+            else if(arr[mid] < k) lt=mid+1;
+            else rt=mid-1;
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+
+        System.out.println(T.solution(arr, k));
+    }
+}
+*/
